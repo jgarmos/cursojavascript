9165042c1776893e86785aaf4a1d94d15ac9365b
Dev1 <dev1@iMac-de-Admin.local>
2019-11-13 13:12:31 +0100

    ejercicio bingo - ocultar bombo y mostrar numero function and  esLineaGanada WIP

--- bingo.js
+++ bingo.js
@@ -5,11 +5,15 @@ const numeroColumnas = 5;
 const numeroCajasVaciasPorFila = 4;
 var posicionesCajaVacia = [];
 var cartonVacioCreado = false;
-
+var bolasSacadas = [];
+var bombo;
+var carton = [];
+var contadorAcertadas = 0;
+var contadorLinea;
 
 function bingo() {
     var tabla = document.getElementById("tabla");
-    var carton = [];
+    
     //var escajaVacia = false;
 
     if(!cartonVacioCreado){
@@ -17,68 +21,101 @@ function bingo() {
         cartonVacioCreado = true;
     }
 
+    bombo = document.createElement("img");
+    bombo.setAttribute("src", "https://thumbs.gfycat.com/InnocentBlackGemsbuck-size_restricted.gif");
+    bombo.style.visibility = "hidden"
+    var divJuego = document.getElementById("juegoContainer");
+    divJuego.appendChild(bombo);
+
     var numerosPorCarton = numeroFilas * numeroColumnas; //numeroFilas * numeroColumnas - (numeroCajasVaciasPorFila - numeroFilas);
     carton = generarNumerosAleatoriosOrdenadosYnoRepetidos(numerosPorCarton);
     rellenarCarton(carton);
 
 
-
-    // for (var i = 0; i < numeroFilas; i++) {
-    //     var fila = document.createEles   ment("tr");
-    //     tabla.appendChild(fila);
-
-    //     // crear cajas vacias
-    //     for (var j = 0; j < numeroCajasVaciasPorFila; j++) {
-    //             var numeroAleat = genererarNumeroAleatorio(0, numeroColumnas-1);
-
-    //             //cuidado con este while puede llegar a ser infinito para cartones grandes
-    //             while (posicionesCajaVacia.includes(numeroAleat)) {
-    //                 numeroAleat = genererarNumeroAleatorio(0, numeroColumnas);
-    //             }
-    //             posicionesCajaVacia.push(numeroAleat);
-    //     }
-
-    //     for (var k = 0; k < numeroColumnas; k++) {
-    //         var columna = document.createElement("td");
-    //         var id = i + "," + k;
-    //         columna.setAttribute("id",id);
-    //         fila.appendChild(columna);
-
-
-    //         //calcular si es caja vacia
-    //         escajaVacia = posicionesCajaVacia.includes(k);
-
-    //         if (!escajaVacia){
-    //             //asignar numero random 
-    //             // generar numero random hasta mientras no esta en mi carton
-    //             var numeroAleatorio = genererarNumeroAleatorio(0, 90);
-
-    //             //cuidado con este while puede llegar a ser infinito para cartones grandes
-    //             while (numeroExisteEnCarton(numeroAleatorio)) {
-    //                 numeroAleatorio = genererarNumeroAleatorio(0, 90);
-    //             }
-    //             carton.push(numeroAleatorio);
-    //             columna.innerHTML = numeroAleatorio;
-    //         }
-    //     }
-    //     posicionesCajaVacia = [];
-    // }
-
-
-    pintarCaja(2, 4);
-    pintarCaja(0, 0);
-    pintarCaja(1, 3);
-    pintarCaja(1, 8);
-    esLineaGanada();
+    // pintarCaja(2, 4);
+    // pintarCaja(0, 0);
+    // pintarCaja(1, 3);
+    // pintarCaja(1, 8);
+    //esLineaGanada();
 
 }
 function sacarBola(){
     // dibujar gif bombo
-    // obtener numero aleatorio
+    bombo.style.visibility = "visible";
+    
+    var bolaSacada;
+    setTimeout(function()
+    { bolaSacada = ocultarBomboYmostrarNumero(bombo); 
+    }, 1000);
+
+    
+
+
+     
+    //  
+
     // ocultar dibujo y muestro el numero
     // guardo numero para controlar repetidos en listaBolasExtraidas 
     //si esta marco mi numero
     // calcular bingo y linea
+
+}
+
+function ocultarBomboYmostrarNumero(bombo){
+    bombo.style.visibility = 'hidden';
+    
+    // obtener numero aleatorio
+    var num_aleatorio = numeroAleatorio = genererarNumeroAleatorio(0, 90);
+    while (numeroExisteEnArray(bolasSacadas, numeroAleatorio)) {
+        numeroAleatorio = genererarNumeroAleatorio(0, 90);
+    }
+    bolasSacadas.push(numeroAleatorio);
+
+    var bola = document.getElementById("bola");
+    bola.innerHTML = numeroAleatorio;
+    bola.style.visibility = "visible";
+
+    //encontrar si el numero esta en mi carton
+    if (numeroExisteEnArray(carton,numeroAleatorio)) {
+
+        contadorAcertadas++;
+        
+        var tds = document.querySelectorAll("td");//document.getElementsByTagName("value",String(numeroAleatorio));
+        var casilla;
+        
+        var numeroEncontrado;
+        var tdIndex = 0;
+        
+        //marcar numero en carton
+        while(!numeroEncontrado){
+            if(tds[tdIndex].innerHTML == numeroAleatorio ){
+                numeroEncontrado = true;
+            }else{
+                tdIndex++;
+            }
+        }
+        tds[tdIndex].setAttribute("class","marcado");
+        // tdIndex = 0;
+        // for (var i = 0; i < tds.length; i++) {
+        //     if()
+        // }
+        
+        
+        //calcular lineas
+        if(esLineaGanada()){
+
+        }
+
+        if (contadorAcertadas == numeroFilas * numeroColumnas){
+            alert(" YOU WINNER!!!!!!!!")
+        }
+        
+
+        
+
+
+    }
+
 
 }
 function rellenarCarton(numerosAleatorios) {
@@ -89,6 +126,7 @@ function rellenarCarton(numerosAleatorios) {
         for (var k = 0; k < numeroColumnas; k++) {
             var element = document.getElementById(i + "," + k);
             element.innerHTML = numerosAleatorios[numerosAleatoriosIndex];
+            element.setAttribute("numeroAsignado",numerosAleatorios[numerosAleatoriosIndex]);             
             numerosAleatoriosIndex++;
         }
     }
@@ -149,8 +187,73 @@ function pintarCaja(fila, columna) {
 
 function esLineaGanada() {
     //cojer elementos que esten marcados en rojo
-    var elementos = document.querySelectorAll("td");
+   
+    var tds = document.querySelectorAll("td");
+    var numerodeFilaInsertados = [];
+    var contadorMaximoLinea = 0;
+    var indexFila = 0;
+    
+    var contadorFilaAux =0;
+    for (let index = 0; index < tds.length; index++) {
+        const element = tds[index];
+        if (element.classList.contains("marcado")){ 
+            indexFila = element.id[0];
+            
+            //numeroFilasInsertados.push(indexFila);
+
+            if(contadorFilaAux > contadorMaximoLinea){
+                contadorMaximoLinea = contadorFilaAux;
+            }
+            if (contadorMaximoLinea == numeroColumnas){
+                return true;
+            }
+        }
+    }
+    return false;
+
+    
 
     //si los elementos marcados en rojo son igual al numero de nemors en line devolver true
 
 }
+
+// for (var i = 0; i < numeroFilas; i++) {
+    //     var fila = document.createEles   ment("tr");
+    //     tabla.appendChild(fila);
+
+    //     // crear cajas vacias
+    //     for (var j = 0; j < numeroCajasVaciasPorFila; j++) {
+    //             var numeroAleat = genererarNumeroAleatorio(0, numeroColumnas-1);
+
+    //             //cuidado con este while puede llegar a ser infinito para cartones grandes
+    //             while (posicionesCajaVacia.includes(numeroAleat)) {
+    //                 numeroAleat = genererarNumeroAleatorio(0, numeroColumnas);
+    //             }
+    //             posicionesCajaVacia.push(numeroAleat);
+    //     }
+
+    //     for (var k = 0; k < numeroColumnas; k++) {
+    //         var columna = document.createElement("td");
+    //         var id = i + "," + k;
+    //         columna.setAttribute("id",id);
+    //         fila.appendChild(columna);
+
+
+    //         //calcular si es caja vacia
+    //         escajaVacia = posicionesCajaVacia.includes(k);
+
+    //         if (!escajaVacia){
+    //             //asignar numero random 
+    //             // generar numero random hasta mientras no esta en mi carton
+    //             var numeroAleatorio = genererarNumeroAleatorio(0, 90);
+
+    //             //cuidado con este while puede llegar a ser infinito para cartones grandes
+    //             while (numeroExisteEnCarton(numeroAleatorio)) {
+    //                 numeroAleatorio = genererarNumeroAleatorio(0, 90);
+    //             }
+    //             carton.push(numeroAleatorio);
+    //             columna.innerHTML = numeroAleatorio;
+    //         }
+    //     }
+    //     posicionesCajaVacia = [];
+    // }
